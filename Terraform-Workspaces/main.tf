62ad61b4d6fef512d86d09cbf9df6fc8312f8541
CamiloBaeza <102620801+CamiloBaeza@users.noreply.github.com>
2022-11-24 20:24:51 +0000

    Automated report

--- Terraform-Workspaces/main.tf
+++ Terraform-Workspaces/main.tf
@@ -1,15 +1,12 @@
 terraform {
-  required_version = ">= 1.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.28"
+      version = ">=4.20.0"
     }
-
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.1"
+    archive = {
+      source  = "hashicorp/archive"
+      version = "2.2.0"
     }
   }
 }
