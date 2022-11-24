8c3a2d6aa64a8b06ed06c5d3d11841b2e53f8928
CamiloBaeza <102620801+CamiloBaeza@users.noreply.github.com>
2022-11-24 02:57:35 +0000

    Automated report

--- aws_s3_bucket/main.tf
+++ aws_s3_bucket/main.tf
@@ -12,20 +12,32 @@ resource "aws_s3_bucket" "srecoursebuf-s3" {
     enabled = true
   }
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 }
-/*
+
 resource "aws_s3_bucket_acl" "example" {
   bucket = aws_s3_bucket.srecoursebuf-s3.id
   acl    = "private"
 }
 
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.srecoursebuf-s3.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+
+
+/*
 resource "aws_s3_bucket_object" "s3_object_estadistica" {
   bucket = "tarrotomado"
   key    = "new_object_key"
